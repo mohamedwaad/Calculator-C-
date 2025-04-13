532b3b2b582ff51584a16f0ae1461d4a1908afe4
mohamedwaad <mohamedwaad12340987@Gmail.com>
2025-04-13 15:15:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string CharacterName = "YALeyay";
+            int CharacterAge = 15;
+
+
+            Console.WriteLine("Hello I am " +CharacterAge+" My Name Is " + CharacterName);
+            Console.WriteLine("Hello I am " + CharacterAge + " My Name Is " + CharacterName);
+            Console.WriteLine("Hello I am " +CharacterAge+" My Name Is " + CharacterName);
+
+        }
+
+
+    }
+}
